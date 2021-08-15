3f744e570d6672c0e123c8bda300146d726a21cb
qNecro <bafordsk@gmail.com>
2021-08-15 19:32:29 +0200

    .

---
+++ BMI-Calculator-LayoutPractice/Controllers/BMI.swift
@@ -0,0 +1,16 @@
+//
+//  BMI.swift
+//  BMI-Calculator-LayoutPractice
+//
+//  Created by Michal on 11/08/2021.
+//  Copyright © 2021 App Brewery. All rights reserved.
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    var color: UIColor
+    
+}
---
+++ BMI-Calculator-LayoutPractice/Controllers/CalculateViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ViewController.swift
+//  BMI-Calculator-LayoutPractice
+//
+//  Created by Angela Yu on 21/08/2019.
+//  Copyright © 2019 App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class CalculateViewController: UIViewController {
+    
+    var calculatorBrain = CalculatorBrain()
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBOutlet weak var heightValue: UILabel!
+    @IBOutlet weak var weightValue: UILabel!
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var weightSlider: UISlider!
+    @IBAction func heightSliderChanged(_ sender: UISlider) {
+        heightValue.text = "\(String(format:"%.2f", sender.value))m"
+        
+        
+    }
+    
+    @IBAction func weightSliderChanged(_ sender: UISlider) {
+        weightValue.text = "\(String(format:"%.0f", sender.value))kg"
+    }
+    @IBAction func calculatePressed(_ sender: UIButton) {
+        let height = heightSlider.value
+        let weight = weightSlider.value
+        
+        calculatorBrain.calculateBMI(height: height, weight: weight)
+        self.performSegue(withIdentifier: "goToResult", sender: self)
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        if segue.identifier == "goToResult" {
+            let destinationVC = segue.destination as! ResultViewController
+            
+            
+            destinationVC.bmiValue = calculatorBrain.getBMIValue()
+            destinationVC.advice = calculatorBrain.getAdvice()
+            destinationVC.color = calculatorBrain.getColor()
+        }
+    }
+    
+
+}
+
